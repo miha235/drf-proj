4d4c1ac14fa7a5b0fdfd7d951014f4257737eb54
Victoria <Victoria23miha@gmail.com>
2025-01-19 20:22:29 +0300

    26.2 Celery

--- config/settings.py
+++ config/settings.py
@@ -137,12 +137,12 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 SITE_URL = 'http://localhost:8000'
 
-'''EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')'''
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 CELERY_BROKER_URL = f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT',6379)}/0"
 CELERY_RESULT_BACKEND = f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/0"
@@ -150,3 +150,11 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = TIME_ZONE
+
+
+CELERY_BEAT_SCHEDULE = {
+    'deactivate-inactive-users': {
+        'task': 'users.tasks.deactivate_inactive_users',
+        'schedule': crontab(hour=0, minute=0),  # Запускать ежедневно в полночь
+    },
+}
--- lms/models.py
+++ lms/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -8,9 +9,21 @@ class Course(models.Model):
     title = models.CharField(max_length=200)
     preview = models.ImageField(upload_to='course_previews', null=True, blank=True)
     description = models.TextField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, default = 1, related_name='courses')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
+    last_updated = models.DateTimeField(auto_now=True)
 
     objects = models.Manager()
+
+    def save(self, *args, **kwargs):
+        # Обновляем last_updated только при изменении других полей
+        if self.pk:
+            orig = Course.objects.get(pk=self.pk)
+            if orig.title != self.title or orig.description != self.description:
+                self.last_updated = timezone.now()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
 
 
 class Lesson(models.Model):
